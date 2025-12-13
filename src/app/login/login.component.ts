7814b953bfa83e86e9a796d77f00ca83cf7205d9
Aleksandar M. <aleksandarmaksimovic257@gmail.com>
2025-12-13 14:53:24 +0100

    implemented form logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = ''; 
+
+  onSubmit() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+    // Here you can add your authentication logic
+  }
 }

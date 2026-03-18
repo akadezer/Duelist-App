921f7d17468ea831292642f3f4e4eeb05da99aad
Aleksandar M. <aleksandarmaksimovic257@gmail.com>
2026-03-18 21:00:47 +0100

    added routes to app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { StartPageComponent } from './start-page/start-page.component';
+import { Sign } from 'crypto';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+];
